cda3ffbaa21e25b9eb16a1f560fb057fa95fd32f
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 12:32:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species, count(id) from characters group by species;"
+  "select species, count(id) from characters group by species order by count(id) desc limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
