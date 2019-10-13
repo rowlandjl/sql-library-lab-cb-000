0ae4e2878bc2f0036a9b0f180583d376243c9370
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 12:34:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "select * from series left join subgenres on series.subgenre_id = subgenres_id"
+  "select * from series left join subgenres on series.subgenre_id = subgeneres.subgenres_id"
 end
 
 def select_series_title_with_most_human_characters
