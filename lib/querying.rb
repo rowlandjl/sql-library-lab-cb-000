47b622146569c585497e4406f906fffd13fff0af
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 12:22:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, min(year) from books left join series on books.series_id = series.id group by books.title order by min(year);"
+  "select books.title, min(year) from books left join series on books.series_id = series.id where series.id = 1 group by books.title order by min(year);"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
