390e7bc00f2df4ebfc4a7ba09a212e7b04de22e4
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 12:39:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select title from series left join authors on series.author_id = authors.id left join characters on authors.id = characters.author_id"
+  "select title from series left join authors on series.author_id = authors.id left join characters on authors.id = characters.author_id where species = "human" group by title "
 end
 
 def select_character_names_and_number_of_books_they_are_in
