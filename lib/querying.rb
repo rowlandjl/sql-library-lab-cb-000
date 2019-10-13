65281cbd620bc049d094be043c518dafa556af99
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-13 12:54:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,5 +26,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select name 
+  from characters 
+  left join character_books on characters.id = character_books.character_id "
 end
